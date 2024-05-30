08dab071047fd561bdaacac753d5eabf0de8d883
AndyMuDz_Dev <andresrios508.ar@gmail.com>
2024-05-30 01:58:58 -0500

    Terminada al 100, depronto despues salgan mejoras

--- client/src/components/CreateGameForm/validation.js
+++ client/src/components/CreateGameForm/validation.js
@@ -2,7 +2,10 @@ const validation = (formData) => {
   const errors = {};
   if (!formData.name) {
     errors.name = 'Name is required';
+  } else if (!/^[a-zA-Z0-9\s]+$/.test(formData.name)) {
+    errors.name = 'The name cannot contain symbols or special characters';
   }
+
   if (!formData.description) {
     errors.description = 'Description is required';
   }
@@ -15,6 +18,8 @@ const validation = (formData) => {
   }
   if (!formData.rating) {
     errors.rating = 'Rating is required';
+  } else if (!/^[1-5]$/.test(formData.rating)) {
+    errors.rating = 'The rating must be a number between 1 and 5';
   }
   if (!formData.genres || formData.genres.length === 0) {
     errors.genres = 'At least one genre is required';
